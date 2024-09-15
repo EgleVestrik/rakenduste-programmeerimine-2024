e6044ca3b33fa633cd644f3a331bfe6d4af40357
egleves <egleves@tlu.ee>
2024-09-15 22:14:45 +0300

    HW: tried Conditional rendering and adding image

--- 2024-09-09/silver-sky/src/App.js
+++ 2024-09-09/silver-sky/src/App.js
@@ -6,10 +6,34 @@ import PropDrilling from "./components/PropDrilling"
 import Show from "./components/Show"
 import Context from "./components/Context"
 
+
 function App() {
   const [show, setShow] = useState(true)
 
   const toggleShow = () => setShow(prevShow => !prevShow)
+
+  const pic1 = {
+    name:"Flowers",
+    src:"/2.jpg",
+    imageSize:90,
+  };
+
+  const people = [
+    { title: 'ME', is: true, id: 1 },
+    { title: 'YOU', is: false, id: 2 },
+    { title: 'US', is: true, id: 3 },
+  ];
+
+  const listItems = people.map(people =>
+    <li
+      key={people.id}
+      style={{
+        color: people.is ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {people.title}
+    </li>
+  );
 
   return (
     <>
@@ -22,6 +46,12 @@ function App() {
       <Counter />
       <Name title="Egle" />
       <Name />
+      <img
+        className="avatar"
+        src={"2.jpg"}
+        alt={'Photo of ' + pic1.name}
+       />
+      <ul>{listItems}</ul>
     </>
   )
 }
