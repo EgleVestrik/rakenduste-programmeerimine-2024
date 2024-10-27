90429bf22c75c01737900c9be6324c203f69e025
egleves <egleves@tlu.ee>
2024-10-27 15:02:57 +0200

    feat: add express-validator

--- 2024.09.23/pink/backend/index.js
+++ 2024.09.23/pink/backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require("cors");
+const { query } = require('express-validator');
 const app = express();
 const port = 8080;
 app.use(cors());
@@ -17,7 +18,14 @@ app.use("/cats", catsRoutes);
 app.get('/', (req, res) => {
   res.send('Hello World Egle!')
 })
+app.get('/hello', query('name').notEmpty(), (req, res) => {
+  const result = validationResult(req);
+  if (result.isEmpty()) {
+    return res.send(`Hello, ${req.query.name}!`);
+  }
 
+  res.send({ errors: result.array() });
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
